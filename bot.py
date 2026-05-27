b9ae75afe7d0f1d42c64a0a0e47c01d42d3780ac
Divakaran_Senthilkumar <109328372+sdivakaran143@users.noreply.github.com>
2026-05-28 00:00:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-
 import os
+
 from dotenv import load_dotenv
+
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -19,9 +20,31 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+print("TOKEN LOADED:", BOT_TOKEN[:10])
+
 app = Application.builder().token(BOT_TOKEN).build()
 
-app.add_handler(CommandHandler("start", start_command))
+
+async def ping(update, context):
+
+    await update.message.reply_text(
+        "pong"
+    )
+
+
+app.add_handler(
+    CommandHandler(
+        "ping",
+        ping
+    )
+)
+
+app.add_handler(
+    CommandHandler(
+        "start",
+        start_command
+    )
+)
 
 app.add_handler(
     MessageHandler(
@@ -31,12 +54,18 @@ app.add_handler(
 )
 
 app.add_handler(
-    CallbackQueryHandler(callback_handler)
+    CallbackQueryHandler(
+        callback_handler
+    )
 )
 
 print("Bot started...")
 
 app.run_polling(
     drop_pending_updates=True,
-    allowed_updates=["message", "callback_query"]
+    allowed_updates=[
+        "message",
+        "callback_query"
+    ],
+    poll_interval=2
 )
